7ae8912549f93af7739c2d24ae6917707c76a242
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-22 15:45:04 +0100

    Create TessBridge.c

---
+++ TessBridge.c
@@ -0,0 +1,15 @@
+#include <jni.h>
+#include <android/log.h>
+
+#define LOG_TAG "TessSDK"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
+JNIEXPORT void JNICALL
+Java_com_tesseract_ui_TessEngine_nativeInit(JNIEnv* env, jobject thiz) {
+    LOGI("Tess Engine Core initialized successfully via JNI.");
+}
+
+JNIEXPORT void JNICALL
+Java_com_tesseract_ui_TessEngine_nativeRenderFrame(JNIEnv* env, jobject thiz) {
+    // Engine frame render loop hook
+}
